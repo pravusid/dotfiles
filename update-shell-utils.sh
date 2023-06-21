edee628cd94622c8321d635d5c845cdc2a2198b3
Sangdon Park <24415130+pravusid@users.noreply.github.com>
2023-06-22 02:26:49 +0900

    update: upgrade shell utils script

--- update-shell-utils.sh
+++ update-shell-utils.sh
@@ -18,6 +18,11 @@ curl -sS https://starship.rs/install.sh | sh
 echo -e "${YELLOW}starship 업데이트 완료${NC}"
 
 asdf update
+asdf plugin update --all
 echo -e "${YELLOW}asdf 업데이트 완료${NC}"
+
+if [[ $(uname) == "Linux" ]]; then
+  curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+fi
 
 echo -e "\n${RED}완료${NC}"
